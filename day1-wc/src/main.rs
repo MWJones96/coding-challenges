0c4c3a0446dddca9843111a680440687a75f5b12
Matthew Jones <mwjones0896@gmail.com>
2026-04-21 23:07:18 +0100

    Day1 - Step 2

--- day1-wc/src/main.rs
+++ day1-wc/src/main.rs
@@ -7,21 +7,36 @@ use std::process::Command;
 #[command(version, about, long_about = None)]
 struct Args {
     #[arg(short = 'c', value_name = "FILE", help = "Get number of bytes in file")]
-    filename: String,
+    filename_bytes: Option<String>,
+
+    #[arg(short = 'l', value_name = "FILE", help = "Get number of lines in file")]
+    filename_lines: Option<String>,
 }
 
 fn main() -> std::io::Result<()> {
     let args = Args::parse();
 
-    // 1. Open the file
-    let mut file = File::open(&args.filename)?;
+    if let Some(filename_bytes) = args.filename_bytes {
+        // 1. Open the file
+        let mut file = File::open(&filename_bytes)?;
+        // 2. Prepare a buffer (String)
+        let mut contents = String::new();
+        // 3. Read into the buffer
+        file.read_to_string(&mut contents)?;
+        let contents = contents.as_bytes().to_vec();
+        println!("  {} {}", get_num_bytes(&contents), &filename_bytes);
+    }
 
-    // 2. Prepare a buffer (String)
-    let mut contents = String::new();
-    // 3. Read into the buffer
-    file.read_to_string(&mut contents)?;
-    let mut contents = contents.as_bytes().to_vec();
-    println!("  {} {}", get_num_bytes(&contents), args.filename);
+    if let Some(filename_lines) = args.filename_lines {
+        // 1. Open the file
+        let mut file = File::open(&filename_lines)?;
+        // 2. Prepare a buffer (String)
+        let mut contents = String::new();
+        // 3. Read into the buffer
+        file.read_to_string(&mut contents)?;
+        let contents = contents.as_bytes().to_vec();
+        println!("    {} {}", get_num_lines(&contents), &filename_lines);
+    }
 
     Ok(())
 }
@@ -30,10 +45,27 @@ fn get_num_bytes(bytes: &Vec<u8>) -> usize {
     bytes.len()
 }
 
+fn get_num_lines(bytes: &Vec<u8>) -> usize {
+    let mut count = 0;
+    for byte in bytes {
+        if *byte == 0x0a {
+            count += 1;
+        }
+    }
+
+    return count;
+}
+
 #[test]
 fn test_num_bytes() {
     let input = include_bytes!("../test.txt");
     assert_eq!(342_190, get_num_bytes(&input.to_vec()));
+}
+
+#[test]
+fn test_num_lines() {
+    let input = include_bytes!("../test.txt");
+    assert_eq!(7_145, get_num_lines(&input.to_vec()));
 }
 
 #[test]
@@ -56,4 +88,26 @@ fn test_output_num_bytes_test2() {
 
     let stdout = str::from_utf8(&output.stdout).expect("Invalid UTF8");
     assert_eq!("  1 test2.txt\n", stdout);
+}
+
+#[test]
+fn test_output_num_lines() {
+    let output = Command::new("cargo")
+        .args(["run", "--", "-l", "test.txt"])
+        .output()
+        .expect("Failed to execute");
+
+    let stdout = str::from_utf8(&output.stdout).expect("Invalid UTF8");
+    assert_eq!("    7145 test.txt\n", stdout);
+}
+
+#[test]
+fn test_output_num_lines_test2() {
+    let output = Command::new("cargo")
+        .args(["run", "--", "-l", "test2.txt"])
+        .output()
+        .expect("Failed to execute");
+
+    let stdout = str::from_utf8(&output.stdout).expect("Invalid UTF8");
+    assert_eq!("    0 test2.txt\n", stdout);
 }
