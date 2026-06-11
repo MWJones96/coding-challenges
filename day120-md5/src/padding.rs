42cd14424d0cd92f6a2b2c9cb13648904cd82ad5
Matthew Jones <mwjones0896@gmail.com>
2026-06-11 18:26:42 +0100

    Refactor padding logic

--- day120-md5/src/padding.rs
+++ day120-md5/src/padding.rs
@@ -1,158 +1,92 @@
 use bitvec::{self, order::Msb0, prelude::*, vec::BitVec};
 
-#[derive(Debug)]
-struct Message {
-    bytes: BitVec<u8, Msb0>,
-    orig_len: u64,
-}
+pub fn add_padding(vec: &mut BitVec<u8, Msb0>) {
+    let orin_len: u64 = vec.len() as u64;
 
-impl Message {
-    fn add_padding_bits(&mut self) {
-        self.bytes.push(true);
-        while self.bytes.len() % 512 != 448 {
-            self.bytes.push(false);
-        }
+    vec.push(true);
+    while vec.len() % 512 != 448 {
+        vec.push(false);
     }
-    fn append_message_len(&mut self) {
-        assert!(self.bytes.len() % 512 == 448);
 
-        let lower = self.orig_len as u32;
-        let upper = (self.orig_len >> 32) as u32;
-
-        self.bytes.extend_from_bitslice(lower.view_bits::<Msb0>());
-        self.bytes.extend_from_bitslice(upper.view_bits::<Msb0>());
-    }
+    vec.extend_from_bitslice(orin_len.to_le_bytes().view_bits::<Msb0>());
 }
 
 #[test]
 fn test_zero_bit_message() {
-    let mut msg: Message = Message {
-        bytes: BitVec::new(),
-        orig_len: 0,
-    };
+    let mut input = BitVec::new();
 
-    assert_eq!(0, msg.orig_len);
-    assert_eq!(0, msg.bytes.len());
+    assert_eq!(0, input.len());
 
-    msg.add_padding_bits();
+    add_padding(&mut input);
 
-    assert_eq!(0, msg.orig_len);
-    assert_eq!(448, msg.bytes.len());
+    assert_eq!(512, input.len());
 
-    msg.append_message_len();
+    let len = input.len();
+    let orig_len: u64 = input[len - 64..].load_le::<u64>();
 
-    assert_eq!(0, msg.orig_len);
-    assert_eq!(512, msg.bytes.len());
-
-    let len = msg.bytes.len();
-    let ms32 = msg.bytes[len - 32..].load_be::<u32>();
-    let ls32 = msg.bytes[len - 64..len - 32].load_be::<u32>();
-
-    assert_eq!(0, ms32);
-    assert_eq!(0, ls32);
+    assert_eq!(0, orig_len);
 }
 
 #[test]
 fn test_one_bit_message() {
-    let mut msg: Message = Message {
-        bytes: bitvec![u8, Msb0; 1; 1],
-        orig_len: 1,
-    };
+    let mut input = bitvec![u8, Msb0; 1; 1];
 
-    assert_eq!(1, msg.orig_len);
-    assert_eq!(1, msg.bytes.len());
+    assert_eq!(1, input.len());
 
-    msg.add_padding_bits();
+    add_padding(&mut input);
 
-    assert_eq!(1, msg.orig_len);
-    assert_eq!(448, msg.bytes.len());
+    assert_eq!(512, input.len());
 
-    msg.append_message_len();
+    let len = input.len();
+    let orig_len: u64 = input[len - 64..].load_le::<u64>();
 
-    assert_eq!(1, msg.orig_len);
-    assert_eq!(512, msg.bytes.len());
-
-    let len = msg.bytes.len();
-    let ms32 = msg.bytes[len - 32..].load_be::<u32>();
-    let ls32 = msg.bytes[len - 64..len - 32].load_be::<u32>();
-
-    assert_eq!(0, ms32);
-    assert_eq!(1, ls32);
+    assert_eq!(1, orig_len);
 }
 
 #[test]
 fn test_447_bit_message() {
-    let mut msg: Message = Message {
-        bytes: bitvec![u8, Msb0; 1; 447],
-        orig_len: 447,
-    };
+    let mut input = bitvec![u8, Msb0; 1; 447];
 
-    assert_eq!(447, msg.orig_len);
-    assert_eq!(447, msg.bytes.len());
+    assert_eq!(447, input.len());
 
-    msg.add_padding_bits();
+    add_padding(&mut input);
 
-    assert_eq!(447, msg.orig_len);
-    assert_eq!(448, msg.bytes.len());
+    assert_eq!(512, input.len());
 
-    msg.append_message_len();
+    let len = input.len();
+    let orig_len: u64 = input[len - 64..].load_le::<u64>();
 
-    assert_eq!(447, msg.orig_len);
-    assert_eq!(512, msg.bytes.len());
-
-    let len = msg.bytes.len();
-    let ms32 = msg.bytes[len - 32..].load_be::<u32>();
-    let ls32 = msg.bytes[len - 64..len - 32].load_be::<u32>();
-
-    assert_eq!(0, ms32);
-    assert_eq!(447, ls32);
+    assert_eq!(447, orig_len);
 }
 
 #[test]
 fn test_448_bit_message() {
-    let mut msg: Message = Message {
-        bytes: bitvec![u8, Msb0; 1; 448],
-        orig_len: 448,
-    };
+    let mut input = bitvec![u8, Msb0; 1; 448];
 
-    assert_eq!(448, msg.orig_len);
-    assert_eq!(448, msg.bytes.len());
+    assert_eq!(448, input.len());
 
-    msg.add_padding_bits();
+    add_padding(&mut input);
 
-    assert_eq!(448, msg.orig_len);
-    assert_eq!(448 + 512, msg.bytes.len());
+    assert_eq!(1024, input.len());
 
-    msg.append_message_len();
+    let len = input.len();
+    let orig_len: u64 = input[len - 64..].load_le::<u64>();
 
-    assert_eq!(448, msg.orig_len);
-    assert_eq!(1024, msg.bytes.len());
-
-    let len = msg.bytes.len();
-    let ms32 = msg.bytes[len - 32..].load_be::<u32>();
-    let ls32 = msg.bytes[len - 64..len - 32].load_be::<u32>();
-
-    assert_eq!(0, ms32);
-    assert_eq!(448, ls32);
+    assert_eq!(448, orig_len);
 }
 
 #[test]
 fn test_large_message() {
-    let mut msg: Message = Message {
-        bytes: bitvec![u8, Msb0; 1; 5_000_000_000],
-        orig_len: 5_000_000_000,
-    };
+    let mut input = bitvec![u8, Msb0; 1; 5_000_000_000];
 
-    msg.add_padding_bits();
-    msg.append_message_len();
+    assert_eq!(5_000_000_000, input.len());
 
-    assert_eq!(5_000_000_000, msg.orig_len);
-    assert!(msg.bytes.len().is_multiple_of(512));
+    add_padding(&mut input);
 
-    let len = msg.bytes.len();
-    let ms32 = msg.bytes[len - 32..].load_be::<u32>();
-    let ls32 = msg.bytes[len - 64..len - 32].load_be::<u32>();
+    assert!(input.len().is_multiple_of(512));
 
-    assert_eq!((msg.orig_len >> 32) as u32, ms32);
-    assert_eq!(msg.orig_len as u32, ls32);
+    let len = input.len();
+    let orig_len: u64 = input[len - 64..].load_le::<u64>();
+
+    assert_eq!(5_000_000_000, orig_len);
 }
