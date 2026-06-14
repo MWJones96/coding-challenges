c0bcca05f30a87be7fd78cde83aeb319fd24160c
Matthew Jones <mwjones0896@gmail.com>
2026-06-14 03:25:35 +0100

    Added more CLI functionality

--- day120-md5/src/main.rs
+++ day120-md5/src/main.rs
@@ -3,11 +3,12 @@ use std::{
     io::{self, Read},
 };
 
-use clap::Parser;
+use clap::{CommandFactory, Parser};
 
 mod process;
 
 #[derive(Parser, Debug)]
+#[command(author, version, about)]
 struct Args {
     #[arg(num_args = 1..)]
     files: Vec<String>,
@@ -15,6 +16,8 @@ struct Args {
 
 fn main() {
     let args = Args::parse();
+    let mut has_error = false;
+
     if args.files.is_empty() {
         let mut buffer = Vec::new();
         io::stdin().read_to_end(&mut buffer).unwrap();
@@ -23,8 +26,24 @@ fn main() {
         println!("{}  -", output);
     } else {
         for file in args.files {
-            let output = process::process(fs::read(&file).unwrap());
-            println!("{}  {}", output, &file);
+            match fs::read(&file) {
+                Ok(bytes) => {
+                    let output = process::process(bytes);
+                    println!("{}  {}", output, &file);
+                }
+                Err(_) => {
+                    eprintln!(
+                        "{}: {}: No such file or directory",
+                        Args::command().get_name(),
+                        &file
+                    );
+                    has_error = true;
+                }
+            }
         }
+    }
+
+    if has_error {
+        std::process::exit(1);
     }
 }
--- day120-md5/tests/integration.rs
+++ day120-md5/tests/integration.rs
@@ -22,3 +22,35 @@ fn test_md5_one_file() {
             "cfa563b916ab0abd03659d0c40aef995  tests/test_file.txt",
         ));
 }
+
+#[test]
+fn test_md5_two_files() {
+    let mut cmd = Command::cargo_bin("day120-md5").unwrap();
+    cmd.args(["tests/test_file.txt", "tests/test_file2.txt"])
+        .assert()
+        .success()
+        .stdout(predicate::eq(
+            "cfa563b916ab0abd03659d0c40aef995  tests/test_file.txt\n\
+             2465ade3580e8edbcf5ed56ed8e1da0c  tests/test_file2.txt\n",
+        ));
+}
+
+#[test]
+fn test_md5_two_files_and_one_file_that_does_not_exist() {
+    let mut cmd = Command::cargo_bin("day120-md5").unwrap();
+    cmd.args([
+        "tests/test_file.txt",
+        "does-not-exist.txt",
+        "tests/test_file2.txt",
+    ])
+    .assert()
+    .failure()
+    .code(1)
+    .stdout(predicate::eq(
+        "cfa563b916ab0abd03659d0c40aef995  tests/test_file.txt\n\
+        2465ade3580e8edbcf5ed56ed8e1da0c  tests/test_file2.txt\n",
+    ))
+    .stderr(predicate::eq(
+        "day120-md5: does-not-exist.txt: No such file or directory\n",
+    ));
+}
